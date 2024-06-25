92b3358b9ee4007930aa653ae2b92c003d75dd82
Nick Schuch <nick@previousnext.com.au>
2024-06-26 09:03:01 +1000

    Move from Goss to a Go script for testing (#36)
    * Move from Goss to a Go script for testing
    
    ---------
    
    Co-authored-by: Nick Schuch <nick@myschuch.com>

---
+++ drupal/tests/test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// Test which will be executed by this function.
+type Test func() error
+
+func main() {
+	var errs []error
+
+	tests := []Test{
+		// Blocking rules.
+		hasStatusCode("http://127.0.0.1:8080/index.PHP", 403),
+		hasStatusCode("http://127.0.0.1:8080/tag", 200),
+		hasStatusCode("http://127.0.0.1:8080/Tag", 404),
+		hasStatusCode("http://127.0.0.1:8080/data.log", 404),
+	}
+
+	for _, test := range tests {
+		err := test()
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Println("FAILED:", err)
+		}
+
+		os.Exit(1)
+	}
+}
+
+// Check if a url has a specified status code.
+func hasStatusCode(url string, code int) Test {
+	return func() error {
+		resp, err := http.Get(url)
+		if err != nil {
+			return fmt.Errorf("failed to get host: %w", err)
+		}
+
+		if resp.StatusCode != code {
+			return fmt.Errorf("url %s does not have corrent status code: have=%d want=%d", url, resp.StatusCode, code)
+		}
+
+		return nil
+	}
+}
